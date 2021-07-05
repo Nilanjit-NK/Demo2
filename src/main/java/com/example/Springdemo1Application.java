19a3c9d5e1ac472764fd2d2b4fcb9c3d6268d74d
Akankshakore <akankshakore1@gmail.com>
2021-07-05 12:45:07 +0530

    first commit

---
+++ src/main/java/com/example/Springdemo1Application.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Springdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springdemo1Application.class, args);
+	}
+	@GetMapping("/")
+	  public String getMsgName() {
+	    return "Welcome Akanksha!";
+	  }
+	@GetMapping("/hi")
+	public String hello() {
+		return "Hello";
+	}
+}
